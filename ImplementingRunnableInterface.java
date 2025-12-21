a2ffdf16f11226bf42cd1e69296dfcf0715142f3
S1mishra <142241537+S1mishra@users.noreply.github.com>
2025-12-21 15:49:36 +0530

    ImplementingRunnableInterface

---
+++ ImplementingRunnableInterface.java
@@ -0,0 +1,17 @@
+//Using a class
+public class ImplementingRunnableInterface impements Runnable{
+    @Override
+    public void run(){
+        System.out.println("Task running!");
+    }
+
+    public static void main(String[] args){
+        Runnable r = new ImplementingRunnableInterface();
+        new Thread(r).start();
+    }
+}
+//Using lambda expression
+/*
+Runnable r = () -> System.out.println("Task running!");
+new Thread(r).start();
+*/
