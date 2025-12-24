d7b55557bef27eb586596db1bf5704a15113a76f
S1mishra <soumya922001@gmail.com>
2025-12-24 08:12:21 +0530

    RenamingClassToIncludeOptionalDouble

--- StreamsAndLambdasBuiltInFunctionalInterfaces/JavaStreamApi_OptionalDouble.java
+++ StreamsAndLambdasBuiltInFunctionalInterfaces/JavaStreamApi_OptionalDouble.java
@@ -1,7 +1,7 @@
 import java.util.OptionalDouble;
 import java.util.stream.IntStream;
 
-public class JavaStreamApi {
+public class JavaStreamApi_OptionalDouble {
     public static void main(String[] args){
         IntStream isl = IntStream.of(1,3,5);
         OptionalDouble ops = isl.filter(i  -> i%2 ==0).average();  
