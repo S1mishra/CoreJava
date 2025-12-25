d6b6fbe6e490e333becd8a9c7232e86011855224
S1mishra <soumya922001@gmail.com>
2025-12-25 12:22:54 +0530

    StaticBlockInitialization

---
+++ ObjectInitializationConceptInJava/CorrectSequencePrintedA.java
@@ -0,0 +1,8 @@
+package ObjectInitializationConceptInJava;
+
+public class CorrectSequencePrintedA {
+    public CorrectSequencePrintedA(){}
+    public CorrectSequencePrintedA(int i){
+        System.out.println(i);
+    }
+}
