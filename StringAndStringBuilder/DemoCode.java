ceef761f7f787aef5fd135b6ab3ffc095d010352
S1mishra <soumya922001@gmail.com>
2026-02-12 22:57:41 +0530

    String and StringBuilder concept

---
+++ StringAndStringBuilder/DemoCode.java
@@ -0,0 +1,17 @@
+public class DemoCode{
+    public static void stringTest(String s){
+        s.replace('h', 's');
+    }
+
+    public static void stringBuilderTest(StringBuilder s){
+        s.append("o");
+    }
+
+    public static void main(String[] args){
+        String s = "hell";
+        StringBuilder sb = new StringBuilder("well");
+        stringTest(s);
+        stringBuilderTest(sb);
+        System.out.println(s + sb);
+    }
+}
